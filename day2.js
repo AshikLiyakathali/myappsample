c6734c95467ae2d2c2213b98249610d8f87b61a9
Ashik Liyakathali <ashik21395@gmail.com>
2019-10-08 13:39:14 -0400

    added day2 code

---
+++ day2.js
@@ -0,0 +1,54 @@
+//variables
+
+var myName;
+
+myName = "Ashik";
+
+var state = "Michigan";
+
+console.log("My name is " + myName);
+console.log("My state is " + state);
+
+state = "California";
+
+console.log("My state is " + state);
+
+//data types
+//boolean
+
+var data = true;
+
+if (data){
+    console.log("Booleans rule!");
+} else {
+    console.log("Booleans are lame.");
+}
+
+var data = false;
+
+if (data){
+    console.log("Booleans rule!");
+} else {
+    console.log("Booleans are lame.");
+}
+
+//null
+
+var n = null;
+console.log(n * 32);
+
+//undefined
+
+var a;
+console.log(a + 2);
+
+//number
+
+var num = 3.6;
+var ber = 6.4;
+console.log(num + ber);
+
+
+
+
+
